5b8854dd38c990ab5b00440598aa6ed901814855
Lingyun_Huang <huangly@shanghaitech.edu.cn>
2022-10-15 16:42:27 -0700

    commit js-intro v0

---
+++ work/js-intro/myScript.js
@@ -0,0 +1,9 @@
+"use strict";
+(function() {
+
+    const openE1 = document.querySelector(".open");
+    openE1.addEventListener("click", function() {
+        openE1.classList.toggle("active");
+    });
+
+})();
